5bff291cfb3d35cf4ee1f275e70aff25b126e2b9
Brendan Paing <54960564+Bpaing@users.noreply.github.com>
2022-04-15 00:00:33 -0700

    Update trackcbz.py
    Can now track how long a file is open.

--- trackcbz.py
+++ trackcbz.py
@@ -1,4 +1,6 @@
 import pickle
+import subprocess
+import time
 
 #I want the scripts to be able to keep track of files I'm currently reading.
 #Every time I open a cbz file, add to a list.
@@ -9,18 +11,40 @@ import pickle
 #Pickle those 4 files for the startup script to read from next time.
 
 class CBZ:
-    filename = ''
-    directory= ''
-    runtime = 0
+    def __init__(self, path):
+        self.path = path
+        self.runtime = 0
+
+    def trackRuntime(self):
+        s = subprocess.Popen([self.path], shell = True)
+        opened = time.time()
+        s.wait()
+        closed = time.time()
+        self.runtime = closed - opened
+    
+    def resetRuntime(self):
+        self.runtime = 0
 
 def readCBZ():
-    i = 0
+    try:
+        pickle_in = open("cbz.pickle", "rb")
+        list = pickle.load(pickle_in)
+        pickle_in.close()
+    except Exception:
+        list = []
+    return list
 
-def writeCBZ():
-    i = 0
+def writeCBZ(data):
+    pickle_out = open("cbz.pickle", "wb")
+    pickle.dump(data, pickle_out)
+    pickle_out.close()
 
-def main():
-    print("Hello world!")
+def trackCBZ():
+    list = readCBZ()
+        
+    print(s.stdout)
+    s.wait()
+    print(s.stdout) 
 
-if __name__ = '__main__':
-    main()
+
+#s = subprocess.Popen([r'E:\Users\Brendan\Downloads\Japanese\Content\読む\妹さえいればいい。\妹さえいればいい。 第14巻.cbz'], shell = True)
