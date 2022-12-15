5ff3da3b3e2140563b46cd3b5e711a1fa982a6a9
Brendan Paing <54960564+Bpaing@users.noreply.github.com>
2022-12-15 00:54:47 -0800

    Add openURL and helper function
    These functions validate and open a HTTP/HTTPS url using the user's default browser.

--- RoutineManager/MVVM/Service/StartupService.cs
+++ RoutineManager/MVVM/Service/StartupService.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using RoutineManager.MVVM.Model;
 
 namespace RoutineManager.MVVM.Service
 {
     public class StartupService
     {
+        private Startup startup;
+        public void openURL(params string[] urlList)
+        {
+            for (int i = 0; i < urlList.Length; i++)
+            {
+                string url = urlList[i];
+                if (checkURLValid(url))
+                    Process.Start(new ProcessStartInfo($"{url}") { UseShellExecute = true });
+            }
+        }
+
+        //Returns true if the given string is a valid HTTP or HTTPS url.
+        private static bool checkURLValid(string str)
+        {
+            Uri uriResult;
+            return Uri.TryCreate(str, UriKind.Absolute, out uriResult) && 
+                (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+        }
+
+        public async Task startProcess()
+        {
+            await Task.Delay(100);
+        }
     }
 }
