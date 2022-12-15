317bdee37857e9113f6427ce1012a9bf611f0e44
Brendan Paing <54960564+Bpaing@users.noreply.github.com>
2022-12-15 00:12:03 -0800

    Update BaseViewModel.cs

--- RoutineManager/MVVM/ViewModel/BaseViewModel.cs
+++ RoutineManager/MVVM/ViewModel/BaseViewModel.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RoutineManager.MVVM.ViewModel
+﻿namespace RoutineManager.MVVM.ViewModel
 {
-    public class BaseViewModel : INotifyPropertyChanged
+    public class BaseViewModel
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+
     }
 }
