cbb7d144969530317970d307d2004ca451a4bfa9
Brendan Paing <54960564+Bpaing@users.noreply.github.com>
2023-03-03 20:08:28 -0800

    Create Navigation Service to replace previous view switching functionality

---
+++ RoutineManager/MVVM/Service/INavigationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RoutineManager.MVVM.ViewModel;
+
+namespace RoutineManager.MVVM.Service
+{
+    public interface INavigationService
+    {
+        void NavigateTo<T>() where T : ViewModelBase;
+    }
+}
